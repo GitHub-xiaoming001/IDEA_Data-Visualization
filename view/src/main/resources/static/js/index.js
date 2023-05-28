b075101d8db8ccec840bcb6e308cc4192687a1ad
ymw <2245993776@qq.com>
2023-05-28 11:24:36 +0800

    第十次提交

--- view/src/main/resources/static/js/index.js
+++ view/src/main/resources/static/js/index.js
@@ -538,4 +538,49 @@
     window.addEventListener("resize", function() {
         myChart.resize();
     });
-})()
+})();
+
+// 饼形图2-南丁格尔玫瑰图
+(function() {
+// 1. 实例化对象
+        var myChart = echarts.init(document.querySelector(".pie2 .chart"));
+// 2.指定配置
+        var option = {
+            legend: {
+                top: 'bottom'
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: { show: true },
+                    dataView: { show: true, readOnly: false },
+                    restore: { show: true },
+                    saveAsImage: { show: true }
+                }
+            },
+            series: [
+                {
+                    name: 'Nightingale Chart',
+                    type: 'pie',
+                    radius: [50, 250],
+                    center: ['50%', '50%'],
+                    roseType: 'area',
+                    itemStyle: {
+                        borderRadius: 8
+                    },
+                    data: [
+                        { value: 40, name: 'rose 1' },
+                        { value: 38, name: 'rose 2' },
+                        { value: 32, name: 'rose 3' },
+                        { value: 30, name: 'rose 4' },
+                        { value: 28, name: 'rose 5' },
+                        { value: 26, name: 'rose 6' },
+                        { value: 22, name: 'rose 7' },
+                        { value: 18, name: 'rose 8' }
+                    ]
+                }
+            ]
+        };
+        // 3. 把配置给实例对象
+        myChart.setOption(option)
+    })();
